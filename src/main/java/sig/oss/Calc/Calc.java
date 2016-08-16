a61bc55c26f753e6232e86712c5b98487c2f8c2a
golsung <golsung@naver.com>
2016-08-16 12:05:46 +0900

    Add add function and test code

---
+++ src/main/java/sig/oss/Calc/Calc.java
@@ -0,0 +1,11 @@
+package sig.oss.Calc;
+
+public class Calc {
+    private int r=0;
+public void add(int x, int y) {
+	r=x+y;
+}
+   public int getResult() {
+	return r;
+}
+}
---
+++ src/test/java/sig/oss/Calc/CalcTest.java
@@ -0,0 +1,15 @@
+package sig.oss.Calc;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CalcTest {
+
+	@Test
+	public void testAdd() {
+		Calc c = new Calc();
+        c.add(10, 20);
+		assertEquals(30, c.getResult());
+	}
+}
+
