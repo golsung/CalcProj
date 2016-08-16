6ff928af174321efe3e1aeca39d5c3da692717ed
golsung <golsung@naver.com>
2016-08-16 12:10:25 +0900

    add subtraction method and test code

--- src/main/java/sig/oss/Calc/Calc.java
+++ src/main/java/sig/oss/Calc/Calc.java
@@ -1,11 +1,15 @@
 package sig.oss.Calc;
 
 public class Calc {
-    private int r=0;
-public void add(int x, int y) {
-	r=x+y;
-}
-   public int getResult() {
-	return r;
-}
+	private int r=0;
+	public void add(int x, int y) {
+		r=x+y;
+	}
+	
+	public void sub(int x, int y) {
+		r=x-y;
+	}
+	public int getResult() {
+		return r;
+	}
 }
--- src/test/java/sig/oss/Calc/CalcTest.java
+++ src/test/java/sig/oss/Calc/CalcTest.java
@@ -11,5 +11,12 @@ public class CalcTest {
         c.add(10, 20);
 		assertEquals(30, c.getResult());
 	}
+	
+	@Test
+	public void testSub() {
+		Calc c = new Calc();
+        c.sub(10, 20);
+		assertEquals(-10, c.getResult());
+	}
 }
 
