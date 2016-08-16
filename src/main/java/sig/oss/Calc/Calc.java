cc11522cb21853bf5da3e20d68592f6c76847d70
golsung <golsung@naver.com>
2016-08-16 12:34:50 +0900

    Update Calc.java

--- src/main/java/sig/oss/Calc/Calc.java
+++ src/main/java/sig/oss/Calc/Calc.java
@@ -2,11 +2,11 @@ package sig.oss.Calc;
 
 public class Calc {
 	private int r=0;
-	public void add(int x, int y) {
+	public void add(int x, int y) { //add method
 		r=x+y;
 	}
 	
-	public void sub(int x, int y) {
+	public void sub(int x, int y) {//subtraction method
 		r=x-y;
 	}
 	public int getResult() {
